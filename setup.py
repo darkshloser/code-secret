ebbecceb7cfba1e93fa58624a5c69b2545e7fdbe
Dobromir Kovachev <dobromir.mail@gmail.com>
2020-05-02 10:26:52 +0300

    Chnages after first test deployment to test.pypi

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="code-secret-DARKSHLOSER",
+    name="code-secret",
     version="0.0.1",
     author="Dobromir Kovachev",
     author_email="dobromir.mail@gmail.com",
     description="Tool for securing sensitive code private/public repositories",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/code-secret",
+    url="https://github.com/darkshloser/code-secret",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
